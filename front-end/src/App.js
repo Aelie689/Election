e9ebdb04d24a7002d69d90256c69372809346f04
Sirapat Eiamassavamongkol <120149572+hlinhhlin@users.noreply.github.com>
2024-11-07 14:00:59 +0700

    NavBar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,31 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import CandidateInfo from "./pages/Candidate_Info"; // เพจของคุณ
-import TestPage from "./pages/TestPage"; // สมมติว่าเพื่อนคุณมีเพจ Test
 import { Button } from "@mui/material";
-import "./App.css";
+// import "./App.css";
 import Map from "./components/Map";
+import LogIn from "./pages/LoginPage";
+import { UserProvider } from "./context/UserContext";
+import { AuthProvider } from "./context/AuthContext";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          {/* เพิ่มลิงก์สำหรับการนำทาง */}
-          <nav>
-            <Link to="/candidate-info" style={{ margin: '0 10px' }}>Candidate Information</Link>
-            <Link to="/test-page" style={{ margin: '0 10px' }}>Test Page</Link>
-          </nav>
-
-          <Routes>
-            {/* กำหนดเส้นทางไปยังแต่ละหน้า */}
-            <Route path="/candidate-info" element={<CandidateInfo />} />
-            <Route path="/test-page" element={<TestPage />} />
-          </Routes>
-        </header>
-        <Map />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <UserProvider>
+          <div>
+            <NavBar />
+            <Routes>
+              <Route path="/candidate-info" element={<CandidateInfo />} />
+              <Route path="/login" element={<LogIn />} />
+            </Routes>
+            <Map />
+          </div>
+        </UserProvider>
+      </Router>
+    </AuthProvider>
   );
 }
 

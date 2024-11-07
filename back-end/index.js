6b94fbb59eb94abd1f1a0d1f590682c71205bbaf
Sirapat Eiamassavamongkol <120149572+hlinhhlin@users.noreply.github.com>
2024-11-07 17:14:52 +0700

    Revert "feat: add cors"

--- back-end/index.js
+++ back-end/index.js
@@ -2,11 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
-const cors = require('cors');
-
-app.use(cors());
-app.use(express.json());
-
 
 const rds_host = process.env.RDS_HOST
 const rds_user = process.env.RDS_USER

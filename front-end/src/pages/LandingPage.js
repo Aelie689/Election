e0b6732206c6d5ce9bfc5b922315eb370361a8c2
Napathsara Pinthong <napathsara.pin@gmail.com>
2024-11-11 02:07:35 +0700

    style: update Landing Page layout

--- front-end/src/pages/LandingPage.js
+++ front-end/src/pages/LandingPage.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Box, Container } from "@mui/material";
 import PartyListRank from "../components/PartyListRank";
-import Province from '../components/Map';
+import Province from "../components/Map";
 
 export default function LandingPage() {
   const [provinceData, setProvinceData] = useState([]);
@@ -12,48 +12,57 @@ export default function LandingPage() {
       try {
         const [partyResponse, provinceResponse] = await Promise.all([
           fetch("http://localhost:8800/get-party-rank"),
-          fetch("http://localhost:8800/get-max-vote-district")
+          fetch("http://localhost:8800/get-max-vote-district"),
         ]);
-  
+
         const partyData = await partyResponse.json();
         const updatedPartyData = partyData.map((party) => ({
           ...party,
           color_code: party.color_code ? `#${party.color_code}` : "#000000",
           total_seat: party.total_seat ?? 0,
         }));
-  
+
         const provinceData = await provinceResponse.json();
-  
+
         setDataPartyListRank(updatedPartyData);
         setProvinceData(provinceData);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
-  
+
     fetchData();
   }, []);
-  
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "row", pt: 4 }}>
-      <Container maxWidth="lg" sx={{background: "pink" }}>
-          <Province data={provinceData} />
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        pt: 4,
+        pb: 10,
+        gap: 10,
+        px: "48px!important",
+      }}
+    >
+      <Container sx={{ px: "0!important" }}>
+        <Province data={provinceData} />
       </Container>
 
       <Container
-        maxWidth="lg"
         sx={{
-          pt: 4,
-          background: "blue",
-          maxHeight: 700, // Set maximum height
-          overflowY: "auto", // Enable vertical scrolling
+          background: "#FCFCFC",
+          maxHeight: 840,
+          overflowY: "auto",
+          borderRadius: "20px",
+          boxShadow: 3,
+          py: "24px!important",
         }}
       >
         {dataPartyListRank.map((party, index) => (
           <PartyListRank key={party.id} data={party} rank={index + 1} />
         ))}
       </Container>
-    </Box>
+    </Container>
   );
 }
